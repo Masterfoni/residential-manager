f1458b8ca1f049dad50c67b5a38ea779025cc3c8
DaniloPSilva <danilo.dpspereira@gmail.com>
2017-10-14 18:54:24 -0300

    feat(Apartamento): #009 cria entidade apartamento

--- src/main/java/ifpe/edu/entities/Apartamento.java
+++ src/main/java/ifpe/edu/entities/Apartamento.java
@@ -13,6 +13,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -20,6 +21,7 @@ import javax.validation.constraints.NotNull;
  * @author DaniloP
  */
 @Entity
+@Table(name = "TB_APARTAMENTO")
 public class Apartamento implements Serializable {
 
     @Id
