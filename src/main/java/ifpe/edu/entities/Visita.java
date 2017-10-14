dab42239d4c2c838973c9e0efb0ba31dfa188393
DaniloPSilva <danilo.dpspereira@gmail.com>
2017-10-14 18:00:46 -0300

    feat(visita): #006 cria entidade visita

---
+++ src/main/java/ifpe/edu/entities/Visita.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifpe.edu.entities;
+
+import java.io.Serializable;
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author DaniloP
+ */
+@Entity
+@Table(name="TB_VISITA")
+@Access(AccessType.FIELD)
+public class Visita implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @NotNull
+    @Column (name="TXT_NOME")
+    protected String nome;
+    
+    @NotNull
+    @Column (name="TXT_CPF")
+    protected String cpf;
+    
+    @NotNull
+    @Column (name="TXT_DESCRICAO")
+    protected String descricao;
+    
+    @OneToOne
+    @JoinColumn (name="ID_USUARIO", referencedColumnName="ID")
+    private Usuario usuario;
+    
+    @NotNull
+    @Column (name="RESERVA")
+    private boolean reserva;
+  
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+    
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+    
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+ 
+    public Usuario getUsuario(){
+        return usuario;
+    }
+    
+    public void setUsuario(Usuario usuario){
+        this.usuario = usuario;
+    }
+     
+    public boolean getReserva(){
+        return reserva;
+    }
+    
+    public void setDescricao(boolean reserva){
+        this.reserva = reserva;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Visita)) {
+            return false;
+        }
+        Visita other = (Visita) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ifpe.edu.entities.Visita[ id=" + id + " ]";
+    }
+    
+}
